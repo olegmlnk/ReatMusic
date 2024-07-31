83140ea4a5d87843db904c387b7fe5fc1fde147a
olegmlnk <olegmelnyk237@gmail.com>
2024-07-31 10:41:30 +0300

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ReatMusic
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ReatMusic
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
